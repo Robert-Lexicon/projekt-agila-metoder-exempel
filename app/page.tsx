b07588dff63a97522c288fa234f5c31bad93c8b2
Robert-Lexicon <robert.hedblad@lexicon.se>
2026-08-21 15:50:24 +0200

    fix: updgrade and audit fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import { getProducts } from "@/lib/api";
 
+// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
+// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
+export const instant = false;
+
 const defaultLimit = "16";
 
 export default async function Home() {
