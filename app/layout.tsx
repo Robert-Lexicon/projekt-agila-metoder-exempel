782a9b139fec0d94a863f16088be1ead8878d964
Robert-Lexicon <robert.hedblad@lexicon.se>
2026-08-26 10:00:21 +0200

    fix: Removed toastlistener since it's not used

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-import { ToastListener } from "@/components/toast-listener";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -31,7 +30,6 @@ export default function RootLayout({
 		>
 			<body className="bg-neutral-100">
 				<Toaster position="bottom-right" />
-				{/* <ToastListener /> */}
 				{children}
 			</body>
 		</html>
